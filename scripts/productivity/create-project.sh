cfec4b48b0d4e055448a16117005d1831f943730
Drew <drew.grubb0@gmail.com>
2018-09-10 22:31:03 -0500

    added create-project

---
+++ scripts/productivity/create-project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Parse Arguments
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        --name)
+            PROJECT_NAME="$2"
+            shift
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$PROJECT_NAME" ]; then
+    echo "A project name under --name is required"
+    exit 1
+fi
+
+cd ~
+mkdir -p Projects
+cd Projects
+mkdir "$PROJECT_NAME"
+
+if [ -x "$(command -v code)" ]; then
+    cd "$PROJECT_NAME"
+    code .
+fi
